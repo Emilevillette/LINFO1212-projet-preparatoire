59664df4ee7da5a9e3032114dd912736a7a32bdb
Emile Villette <emile.villette@student.uclouvain.be>
2022-10-07 12:07:30 +0200

    fixed logo

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var express = require('express');
+var path = require('path');
 var app = express();
+
+var public_dir = path.join(__dirname, 'public');
 
 app.set('view engine', 'ejs');
 
@@ -14,6 +17,8 @@ app.get('/login', function (req, res) {
 app.get('/incident_input', function (req, res) {
     res.render('pages/incident_input.ejs');
 });
+
+app.use(express.static(public_dir));
 
 app.use(express.static('content'));
 app.listen(8080);
--- src/js/insert_code.js
+++
@@ -1,7 +0,0 @@
-function loadContent(divID, file) {
-    fetch(file)
-        .then(response => response.text())
-        .then(text => document.getElementById(divID).innerHTML = text);
-}
-
-// Inspired by: https://www.delftstack.com/howto/javascript/load-html-file-javascript/

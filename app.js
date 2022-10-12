4b2e70ec1bc71b34e00e67fd6fae0a7fe22c35d5
Emile Villette <emile.villette@student.uclouvain.be>
2022-10-12 16:44:50 +0200

    db init

--- app.js
+++ app.js
@@ -2,9 +2,23 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
+const db = require("./config/database");
+
 var public_dir = path.join(__dirname, 'public');
 
 app.set('view engine', 'ejs');
+
+const initDB = async () => {
+    console.log("Starting database:");
+    try {
+        await db.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+
+initDB();
 
 app.post('/login_account', function (req, res, next) {
 
@@ -34,4 +48,8 @@ app.get('*', function (req, res) {
 
 
 app.use(express.static('content'));
-app.listen(8080);
+app.listen(8080, () => {
+    console.log("Server up at http://localhost:8080/")
+});
+
+

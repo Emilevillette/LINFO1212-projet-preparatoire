036f9a5b002bb40e5a14cdb7f6dfde716f862dd3
Emile Villette <emile.villette@student.uclouvain.be>
2022-10-21 12:20:07 +0200

    store database in file and ignore

--- config/database.js
+++ config/database.js
@@ -1,6 +1,9 @@
 const {Sequelize} = require('sequelize');
 
 // Option 1: Passing a connection URI
-const sequelize = new Sequelize('sqlite::memory:') // Example for sqlite
+const sequelize = new Sequelize({
+    dialect: "sqlite",
+    storage: "preparatoryprojet.sqlite"
+})
 
 module.exports = sequelize;
